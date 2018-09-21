45093aafb2bff8f860011b8f706d29f5be782f2d
Nikita <nikita-bat2000@mail.ru>
2018-09-21 20:53:41 +0500

    done

--- tmp6/tmp6.py
+++ tmp6/tmp6.py
@@ -1,6 +1,10 @@
 a = input()
-b = []
-for i in range(len(a)//7):
-    b.append(sum(list(int(a[x]) for x in range(i*6,i*6+7 ) )))
+b = list(sum(list(int(a[x]) for x in range(i*7,i*7+7))) for i in range(len(a)//7))
+if len(a)%7!=0:
+    b.append(sum(list(int(a[i]) for i in range(len(b)*7,len(a)))))
 print(b)
 print(max(b))
+
+
+#for i in range(len(a)//7):
+#    b.append(sum(list(int(a[x]) for x in range(i*7,i*7+7))))
